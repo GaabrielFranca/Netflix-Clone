c6555cf5d055aaefbc83ff738f06153c55e2a904
Gabriel França <gabrielfran1864@gmail.com>
2022-11-08 23:37:32 -0300

    HomeScreen (Navbar && Banner)

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./Banner";
+import NavBar from "./NavBar";
+
+const HomeScreen = () => {
+  return (
+    <main>
+      <NavBar />
+      <Banner />
+    </main>
+  );
+};
+
+export default HomeScreen;
