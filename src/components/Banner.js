afd11053dacc32cf31b37690597fab89364ecd8b
Gabriel França <gabrielfran1864@gmail.com>
2022-11-10 19:19:30 -0300

    updating banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,36 +3,49 @@ import styles from "./Banner.module.css";
 import requests from "../Requests";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(requests.fetchNetflixOriginals);
+      const json = await res.json();
+
+      setMovie(
+        json.results[Math.floor(Math.random() * json.results.length - 1)]
+      );
+    }
+
+    fetchData();
+  }, []);
+
   function counterLetter(string, count) {
     return string.length > count ? string.substr(0, count - 1) + "..." : string;
   }
 
-  return (
-    <header
-      className={styles.Banner}
-      style={{
-        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`,
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className={styles.Banner__contents}>
-        <h1 className={styles.Banner__title}>Title Movie</h1>
-        <div className={styles.Banner__buttons}>
-          <button className={styles.Banner__button}>Play</button>
-          <button className={styles.Banner__button}>My List</button>
+  if (movie) {
+    return (
+      <header
+        className={styles.Banner}
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`,
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className={styles.Banner__contents}>
+          <h1 className={styles.Banner__title}>{movie.name}</h1>
+          <div className={styles.Banner__buttons}>
+            <button className={styles.Banner__button}>Play</button>
+            <button className={styles.Banner__button}>My List</button>
+          </div>
+          <h1 className={styles.Banner__description}>
+            {counterLetter(`${movie.overview}`, 150)}
+          </h1>
         </div>
-        <h1 className={styles.Banner__description}>
-          {counterLetter(
-            " asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada asdasdas asdasda asdada",
-            150
-          )}
-        </h1>
-      </div>
 
-      <div className={styles.Banner__fadeBottom}></div>
-    </header>
-  );
+        <div className={styles.Banner__fadeBottom}></div>
+      </header>
+    );
+  }
 };
-
 export default Banner;
