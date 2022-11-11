9bfff1fd4ab59d5e9133e3f5ed83f3e38410cce6
Gabriel França <gabrielfran1864@gmail.com>
2022-11-11 17:09:45 -0300

    Add firebase

--- axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL:
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=${_KEYMOVIES}&language=en-US",
-});
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_KEY_FIREBASE,
+  authDomain: "netflix-build-clone-acd10.firebaseapp.com",
+  projectId: "netflix-build-clone-acd10",
+  storageBucket: "netflix-build-clone-acd10.appspot.com",
+  messagingSenderId: "1009456186537",
+  appId: "1:1009456186537:web:258e3ac67dea0bb2e715b8",
+};
+const app = initializeApp(firebaseConfig);
