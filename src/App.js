a69e5c87864ce6fb02761e4d684a13a317100622
Gabriel França <gabrielfran1864@gmail.com>
2022-11-14 22:44:19 -0300

    redirect authenticated user to main page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/LoginScreen";
+import { logout, login, selectUser } from "./features/user/userSlice";
 import { auth } from "./firebase.js";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        console.log("Nehum usuario logado");
+        dispatch(logout);
       }
     });
 
